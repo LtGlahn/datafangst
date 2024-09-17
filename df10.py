8caf8e95a44e04a661c42fddcfdf0d41dfdaf2b9
LtGlahn <jensen72@gmail.com>
2024-09-17 12:39:44 +0200

    Kan opprette ny og erstatte eksisterende featureCollection på kontrakt

--- df10.py
+++ df10.py
@@ -55,11 +55,10 @@ def alleKontrakter(  url='https://datafangst.vegvesen.no/api/v1/contract/', user
     if not isinstance( user, str): 
         user = input( "Datafangst eller NVDB brukernavn:")
 
-    if not isinstance( pw, str): 
-        pw = hentPassord( user, url )
-
     contracts = get_data( url, user=user, pw=pw )
     return contracts
+
+
 
 def searchContracts( contractList:dict, substring:str): 
     """
@@ -115,14 +114,169 @@ def alleFeaturecollections( contractId:str, destination='NVDB', api='https://dat
     return data 
 
 
+def sjekkStatus( DF10Respons, user='jajens', pw=None, utskrift=True ):  
+    """
+    Følger status-lenken som finnes i responsen fra df10.postFeatureCollection eller df10.putFeatureCollection 
+
+    ARGUMENTS
+        DF10Respons - dictionary med respons fra  df10.postFeatureCollection eller df10.putFeatureCollection
+
+    KEYWORS
+        user - str, brukernavn som har skriverettigheter på kontrakten. Mitt brukernavn "jajens" er satt som default, men det overstyrer du. 
+                    Sett user=None for å oppgi brukernavn interaktivt i python shell. 
+
+        pw - str, passord med leserettigheter på kontrakten. Hvis blankt blir du interaktivt spurt i python shell. 
+    
+        utskrift - boolean, default = True. Sett lik false for å skru av logging til konsoll 
+    Returns 
+        dictionary med detaljert statusinformasjon. 
+
+        Liste med dictionary dersom det er flere enn ett statuselement i responsen (mot formodning).
+
+        None hvis alt feiler
+    """   
+
+    assert 'resources' in DF10Respons, 'mangler "resources" element'
+    assert isinstance( DF10Respons['resources'], list) , '"resources" element er ikke en liste'
+    assert len( DF10Respons['resources']) > 0, '"resources" listen har ingen elementer'
+    assert 'rel' in DF10Respons['resources'][0], 'Elementene i "resource" listen mangler "rel" - element' 
+    assert 'src' in DF10Respons['resources'][0], 'Elementene i "resource" listen mangler "src" - element' 
+    assert 'featureCollectionId' in DF10Respons, 'DF10Respons mangler FeatureCollectionID'
+
+    statusElement = [ x for x in DF10Respons['resources'] if x['rel'] == 'status']
+    assert len( statusElement) > 0, f'Finner ikke statuselement'
+    assert 'src' in statusElement[0], 'Statuselement mangler "src" - lenke' 
+
+    print( f"Henter status for {len( statusElement)} for feature Collection {DF10Respons['featureCollectionId']}")
+    myList = []
+    for enStatus in statusElement: 
+        minStatus = get_data( enStatus['src'] )
+        if utskrift: 
+            print( minStatus )
+        myList.append( minStatus)
+
+    if len( myList ) == 0: 
+        print( f"Fant ikke noe gyldig statuselement??? ")
+    elif len( myList) < 2: 
+        return myList[0]
+    else: 
+        print( f"Fant totalt {len(myList)} status-element, det var rart??? \n\n")
+        return myList
+
+
+def postFeatureCollection( contractId:str, data:dict, api='https://datafangst.vegvesen.no/api/v1/contract/', user='jajens', pw=None, **kwargs ): 
+    """
+    Sender inn datafangst geojson til kontrakten  
+
+    Ref https://apiskriv.vegdata.no/datafangst/datafangst-api#post-feature-collection-til-kontrakt 
+
+    Returnerer en json dictionary med metadata som har ID og lenke til status og valideringsinformasjon. Merk at 
+    lagreprosessen er asynkron. Responsen fra API er alltid http 202 OK, men data kan likevel bli avvist 
+    i valideringsprosess. Du må selv bruke lenkene i responsen for å sjekke eventuelle feilkoder, evt bruk funksjonen 
+    df10.sjekkstatus 
+
+    ARGUMENTS: 
+         contractId - str, ID til kontrakten som vi skal sende data til  
+         
+         data - dict, Datafangst 1.0 geojson  https://apiskriv.vegdata.no/datafangst/datafangst-api#format 
+    
+    KEYWORDS: 
+        api - str, default='https://datafangst.vegvesen.no/api/v1/contract/' . Kan byttes ut med API-lenke til TESTPROD (atm) eller utviklingsmiljø
+         
+        user - str, brukernavn som har skriverettigheter på kontrakten. Mitt brukernavn "jajens" er satt som default, men det overstyrer du. 
+                    Sett user=None for å oppgi brukernavn interaktivt i python shell. 
+        
+        pw=None - str, passord med skriverettigheter på kontrakten. Hvis blankt blir du interaktivt spurt i python shell.  
+        
+    RETURNS 
+        dictionary med responsen fra API. Inni denne responsen finner du lenke for å sjekke status. Se funksjon df10.sjekkStatus 
+    """
+
+
+    headers = { 'Content-Type' : 'application/geo+json', 'Accept' : 'application/json' }
+
+    if not isinstance( user, str): 
+        user = input( "Datafangst eller NVDB brukernavn:")
+
+    if not isinstance( pw, str): 
+        pw = hentPassord( user, api )
+
+    url = api + contractId + '/featurecollection'
+    r = requests.post( url, json=data, headers=headers, auth=HTTPBasicAuth(user, pw))
+
+    if r.ok: 
+        print( f"Vellykket innsending av geojson på kontrakt {contractId} HTTP post {r.status_code}" )
+        apiRespons = r.json()
+        print( f"FeatureCollection ID: {apiRespons['featureCollectionId']}")
+        return apiRespons
+    else: 
+        print( f"Innsending av geojson http POST feilet {r.status_code} {r.text[0:500]}")        
+        from IPython import embed; embed()
+
+
+
+def putFeatureCollection( contractId:str, featureCollectionID:str, data:dict, api='https://datafangst.vegvesen.no/api/v1/contract/', user='jajens', pw=None, **kwargs ): 
+    """
+    Overskriver en eksisterende geojson featureCollection på kontrakten. 
+
+    Ref https://apiskriv.vegdata.no/datafangst/datafangst-api#erstatt-feature-collection
+
+
+    Returnerer en json dictionary med metadata som har ID og lenke til status og valideringsinformasjon. Merk at 
+    lagreprosessen er asynkron. Responsen fra API er alltid http 202 OK, men data kan likevel bli avvist 
+    i valideringsprosess. Du må selv bruke lenkene i responsen for å sjekke eventuelle feilkoder, evt bruk funksjonen 
+    df10.sjekkstatus 
+
+    
+    ARGUMENTS: 
+         contractId - str, ID til kontrakten som vi skal sende data til  
+
+          featureCollectionID - str, ID til featureCollection som skal overskrives
+         
+         data - dict, Datafangst 1.0 geojson  https://apiskriv.vegdata.no/datafangst/datafangst-api#format 
+    
+    KEYWORDS: 
+        api - str, default='https://datafangst.vegvesen.no/api/v1/contract/' . Kan byttes ut med API-lenke til TESTPROD (atm) eller utviklingsmiljø
+         
+        user - str, brukernavn som har skriverettigheter på kontrakten. Mitt brukernavn "jajens" er satt som default, men det overstyrer du. 
+                    Sett user=None for å oppgi brukernavn interaktivt i python shell. 
+        
+        pw=None - str, passord med skriverettigheter på kontrakten. Hvis blankt blir du interaktivt spurt i python shell.  
+        
+    RETURNS 
+        dictionary med responsen fra API. Inni denne responsen finner du lenke for å sjekke status. Se funksjon df10.sjekkStatus 
+    """
+
+    headers = { 'Content-Type' : 'application/geo+json', 'Accept' : 'application/json' }
+
+    if not isinstance( user, str): 
+        user = input( "Datafangst eller NVDB brukernavn:")
+
+    if not isinstance( pw, str): 
+        pw = hentPassord( user, api )
+
+    url = api + contractId + '/featurecollection/'  + featureCollectionID
+    r = requests.put( url, json=data, headers=headers, auth=HTTPBasicAuth(user, pw))
+
+    if r.ok: 
+        print( f"Vellykket overskriving av geojson på kontrakt {contractId} HTTP put {r.status_code} {r.text[0:500]}")
+        apiRespons = r.json()
+        print( f"FeatureCollection ID: {apiRespons['featureCollectionId']}")
+        return apiRespons
+
+    else: 
+        print( f"Innsending av geojson http PUT feilet {r.status_code} {r.text[0:500]}")        
+        from IPython import embed; embed()
+
+
 def lagreFeatureCollections( contractId:str, mappenavn:str, api='https://datafangst.vegvesen.no/api/v1/contract/', user='jajens', pw=None, **kwargs): 
     """
-    Lagrer alle featureCollections på en kontrakt i angitt mappenavn 
+    Laster ned og lagrer alle featureCollections som finnes på angitt kontrakt til det angitte mappenavn 
 
     ARGUMENTS: 
         contractId : ID på kontrakt  
 
-        mappenavn : tekst, mappenavn dit du lagrer mappene 
+        mappenavn : tekst, navn på den mappen der du vil lagre filene.  
 
     KEYWORDS
         Samme som alleFeatureCollections 
@@ -163,19 +317,21 @@ def lagreFeatureCollections( contractId:str, mappenavn:str, api='https://datafan
 
     print( f"Tidsbruk totalt: {datetime.now()-t0}")
 
-
+# Eksempel på interaktiv bruk 
 if __name__ == '__main__': 
     api = 'https://datafangst.vegvesen.no/api/v1/contract/'
     # Kontrakt som har problem: 
-    frankContractId = '7877472b-190f-4898-9939-82915952e15f'
+    problemkontrakt = '7877472b-190f-4898-9939-82915952e15f'
     pw = hentPassord( 'jajens', api)
 
 
     featureCollectionList = ['2143c586-d067-4ef3-824a-1483a6c9db86', '51006529-b1cc-467f-9391-74da2bd2380e', '839c7c9e-31ad-4430-b56a-4074399b021e', '9750b500-fce0-4073-b20d-a3088a1aeaf4', 'b73d64c9-1e62-4c67-a14d-61110457fa25' ]
     for featCol in featureCollectionList: 
-        myStatus = get_data( api + frankContractId + '/featurecollection/' + featCol + '/status' , pw=pw)
+        myStatus = get_data( api + problemkontrakt + '/featurecollection/' + featCol + '/status' , pw=pw)
         print( myStatus)
 
+# Eksempler på valideringsresultat (som inneholder en blanding av blokkerende feil og ikke-blokkerende advarsler og notabene)
+# 
 # '2143c586-d067-4ef3-824a-1483a6c9db86', 
 # 'validationStatus': 'PENDING', 
 # 'validationIssues': [{'severity': 'NOTABENE', 'code': 'KOORDINATER_TRANSFORMERT', 'message': 'Koordinater ble transformert til UTM33N.',
